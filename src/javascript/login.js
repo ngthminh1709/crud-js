34e352518ac346a353067686d02ec820fb6b8520
minhngth2409 <minhngth2409@gmail.com>
2022-09-16 19:04:12 +0700

    fix login

--- src/javascript/login.js
+++ src/javascript/login.js
@@ -1,9 +1,11 @@
 import Method from "./method.js";
+import Validator from "./validate.js";
 import { STUDENTS_PAGE } from "./constant.js";
 const { getUser } = new Method();
 const $ = document.querySelector.bind(document);
 
 const loginBtn = $(".btn-login");
+const loginForm = $("#form-login");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const messageError =
@@ -14,9 +16,14 @@ if (user) {
   window.location.href = STUDENTS_PAGE;
 }
 
-const handleLogin = async () => {
+const handleLogin = async (e) => {
+  e.preventDefault();
+
   const email = emailInput.value;
   const password = passwordInput.value;
+
+  if (!email || !password) return;
+
   const data = getUser();
 
   data
@@ -46,6 +53,15 @@ const handleLogin = async () => {
     });
 };
 
-loginBtn.addEventListener("click", () => {
-  handleLogin();
+loginForm.addEventListener("submit", handleLogin);
+
+
+Validator({
+  form: '#form-login',
+  formGroupSelector: '.form-group',
+  errorSelector: '.form-message',
+  rules: [
+    Validator.isEmail('#email'),
+    Validator.isRequired('#password', 'Vui Lòng Nhập Mật Khẩu')
+  ],
 });
